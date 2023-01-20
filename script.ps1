2760c58dc2e883eae3447c2081a95635a656534f
AzpektDev <eryk.janiczek@gmail.com>
2023-01-20 22:01:04 +0100

    transcript

--- script.ps1
+++ script.ps1
@@ -1,2 +1,6 @@
+Start-Transcript -Path "C:\transcript.txt"
+
 $dir = [Environment]::GetFolderPath("Desktop")
 New-Item -Path $dir -Name "got_hacked_lmao.txt" -Value "Azpekt was here lol"
+
+Stop-Transcript
