556fcc2d61dbbdcfb4d302ad6b3dc668d193b578
AzpektDev <eryk.janiczek@gmail.com>
2023-01-20 23:07:14 +0100

    remove transcript

--- script.ps1
+++ script.ps1
@@ -1,6 +1,2 @@
-Start-Transcript -Path "C:\transcript.txt"
-
 $dir = [Environment]::GetFolderPath("Desktop")
 New-Item -Path $dir -Name "got_hacked_lmao.txt" -Value "Azpekt was here lol"
-
-Stop-Transcript
